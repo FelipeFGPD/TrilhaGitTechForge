8aa792c2bb3d57545a4b59550b22685ad6cefea7
FelipeFGPD <fgp.damasio@gmail.com>
2024-04-14 00:50:18 -0300

    adicionando segundo exercicio

---
+++ exercicio2.js
@@ -0,0 +1,12 @@
+
+function verificaNumero(numero) {
+    if (numero % 2 === 0) {
+        console.log(numero + " é par.");
+    } else {
+        console.log(numero + " é ímpar.");
+    }
+}
+
+verificaNumero(4);
+verificaNumero(7);
+verificaNumero(0);
